1d67d58d8b454f07d03d8fc0691115194b40357a
aymaneab00 <soquavo@gmail.com>
2023-11-01 22:49:24 +0000

    excercice react learning

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import './App.css'
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text:
+      "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text:
+      "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
+  }
+];
+
+export default function App() {
+  return (
+    <div>
+      <Accordion faqs={faqs} />
+    </div>
+  );
+}
+
+function Accordion({faqs}) {
+  return <div>
+{faqs.map(
+
+   (f,i)=> <AccordionItem num={i} title={f.title} text={f.text} />)}
+  </div>;
+}
+function AccordionItem({num,title,text}){
+  function handleToogle (){
+    setIsOpen(isOpen=>!isOpen)
+  }
+  const [isOpen,setIsOpen]=useState(false);
+return(
+  <div className={`item ${isOpen ? 'open': ''}`} onClick={handleToogle}>
+    <p className="number">{num}</p>
+    <p className="text">{title}</p>
+    <p className="icon">{isOpen ? "+" : "-"}</p>
+    { isOpen && <div className="content-box">{text}</div>}
+
+  </div>
+)
+}
